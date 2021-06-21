3aa890909badf4e2450229a855741a1bc8e2283e
Lucas Alves <as.lucasalves@gmail.com>
2021-06-21 01:19:52 -0300

    js script encurtador feito

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,31 @@
-const $linkUser = document.querySelector("#search");
+const $urlUser = document.querySelector("#url-user");
 
-const $btnSubmit = document.querySelector("#submit");
+const $shortenBtn = document.querySelector(".shorten-btn");
 
 const $short_link = document.querySelector("#short_link");
 
-$btnSubmit.addEventListener("click", () => {
-    console.log($linkUser.value)
-  fetch(`https://api.shrtco.de/v2/shorten?url=${$linkUser.value}`)
-    .then((res) => {
-        return res.json();
-    })
-    .then((dados) => {
-      $short_link.innerHTML = dados.result["short_link"];
+
+$shortenBtn.addEventListener("click", () => {
+  $short_link.innerHTML = "Gerando sua url, aguarde..."
+  // console.log($urlUser.value)
+  fetch(`https://api.shrtco.de/v2/shorten?url=${$urlUser.value}`)
+  .then((res) => {
+    return res.json();
+  })
+  .then((dados) => {
+    // console.log(dados.result["short_link"])
+    $short_link.innerHTML = dados.result["short_link"];
+    let urlEncurtada = dados.result["short_link"]
+    // console.log( urlEncurtada)
+    return urlEncurtada
     });
+    
 });
+
+
+const $btnCopy = document.querySelector("#btn-short-url");
+
+$btnCopy.addEventListener("click",()=>{
+
+})
+
