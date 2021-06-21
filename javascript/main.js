fb16441eb6eb7533b48a71a715a4214a94a4c148
Lucas Alves <as.lucasalves@gmail.com>
2021-06-21 11:47:38 -0300

    erro no clipboard

--- javascript/main.js
+++ javascript/main.js
@@ -4,28 +4,48 @@ const $shortenBtn = document.querySelector(".shorten-btn");
 
 const $short_link = document.querySelector("#short_link");
 
+const $btnCopy = document.querySelector("#btn-short-url");
+
+let urlNova = "";
+
+// function criaHistorico(){
+//   const div = `
+//     <div class="url-shortened-box">
+//       <div id="short_link"><p>Sua URL encurtada!</p></div>
+//       <button id="btn-short-url"><i class="far fa-copy"></i></button>
+//     </div>`
+
+//   document.querySelector("#start-target").innerHTML += div
+// }
 
 $shortenBtn.addEventListener("click", () => {
+  console.log($urlUser.value)
   $short_link.innerHTML = "Gerando sua url, aguarde..."
-  // console.log($urlUser.value)
+  
   fetch(`https://api.shrtco.de/v2/shorten?url=${$urlUser.value}`)
   .then((res) => {
     return res.json();
   })
   .then((dados) => {
-    // console.log(dados.result["short_link"])
+    
     $short_link.innerHTML = dados.result["short_link"];
-    let urlEncurtada = dados.result["short_link"]
-    // console.log( urlEncurtada)
-    return urlEncurtada
+    let urlEncurtada = dados.result["short_link"];
+    console.log(`Nova url encurtada é ${urlEncurtada}`);
+    urlNova = urlEncurtada;
+    return urlEncurtada;
+    
     });
     
+    
+    $btnCopy.addEventListener('click', ()=>{
+      console.log(urlNova);
+      urlNova.select(); //não estou conseguindo passar o metodo .select
+      document.execCommand('copy');
+    })
 });
 
 
-const $btnCopy = document.querySelector("#btn-short-url");
 
-$btnCopy.addEventListener("click",()=>{
 
-})
+
 
